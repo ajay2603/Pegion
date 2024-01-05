fde19b9480764d221671921b127f8ea1176f6a0f
ajay2603 <j.a.r.raju2003@gmail.com>
2024-01-05 14:32:33 +0530

    done with effects in callRoom

--- frontend/src/components/calls/CallRoom.jsx
+++ frontend/src/components/calls/CallRoom.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function CallRoom() {
   const [chatUser, setChatUser] = useState("ajay");
   const [videoOn, setVideoOn] = useState(true);
   const [micOn, setMicOn] = useState(true);
+
+  const [isDisp, setIsDisp] = useState(true);
 
   const handelVidoOnOff = () => {
     setVideoOn(!videoOn);
@@ -13,20 +15,35 @@ function CallRoom() {
     setMicOn(!micOn);
   };
 
+  const handleDisp = () => {
+    setIsDisp(!isDisp);
+  };
+
   return (
     <div className="flex justify-center">
-      <div className=" text-white absolute top-[-5vh] flex justify-center items-center h-[15vh] w-full bg-gradient-to-b from-black to-transparent">
+      <div
+        className={`text-white absolute flex justify-center items-center h-[15vh] w-full bg-gradient-to-b from-black to-transparent transition-all duration-500 ${
+          !isDisp ? " translate-y-[-20vh]" : " translate-y-[-5vh]"
+        } `}>
         <label className=" relative top-[2vh] text-2xl logsupTxt">
           {chatUser}
         </label>
       </div>
-      <video className=" flex h-screen w-screen bg-blue-500"></video>
-      <video className=" absolute top-[10vh] right-4 aspect-video md:h-24 h-20 bg-green-500"></video>
-      <div className=" flex justify-center items-center h-[15vh] w-full absolute bottom-0 gap-6">
+      <video
+        className=" flex h-screen w-screen bg-blue-500"
+        onClick={handleDisp}></video>
+      <video
+        className={`absolute ${
+          isDisp ? "top-[10vh]" : "top-4"
+        } right-4 aspect-video md:h-24 h-20 bg-green-500 transition-all duration-500`}></video>
+      <div
+        className={`text-white absolute bottom-0 flex justify-center h-[20vh] w-full gap-6 bg-gradient-to-t from-black to-transparent transition-all duration-500  ${
+          !isDisp ? " translate-y-[20vh]" : " translate-y-[5vh]"
+        } `}>
         <div
           className={`${
             videoOn ? "bg-gray-600" : " bg-white "
-          }  h-14 w-14 flex justify-center items-center rounded-[50%] cursor-pointer`}
+          }  h-14 w-14 flex justify-center items-center rounded-[50%] cursor-pointer relative bottom-[-3vh]`}
           onClick={handelVidoOnOff}>
           <span
             className={`material-symbols-outlined text-3xl font-medium ${
@@ -35,7 +52,7 @@ function CallRoom() {
             videocam_off
           </span>
         </div>
-        <div className=" bg-red-500 h-14 w-14 flex justify-center items-center rounded-[50%] cursor-pointer">
+        <div className=" bg-red-500 h-14 w-14 flex justify-center items-center rounded-[50%] cursor-pointer relative bottom-[-3vh]">
           <span className="material-symbols-outlined text-3xl font-medium text-white">
             phone_disabled
           </span>
@@ -43,7 +60,7 @@ function CallRoom() {
         <div
           className={`${
             micOn ? "bg-gray-600" : " bg-white "
-          }  h-14 w-14 flex justify-center items-center rounded-[50%] cursor-pointer`}
+          }  h-14 w-14 flex justify-center items-center rounded-[50%] cursor-pointer relative bottom-[-3vh]`}
           onClick={handelMicOnOff}>
           <span
             className={`material-symbols-outlined text-3xl font-medium ${
